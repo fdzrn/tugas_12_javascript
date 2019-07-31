245c2bdf427985a9af8dca554b7cdf44ac545a2f
fdzrn <fadzriangibran@gmail.com>
2019-07-31 17:28:11 +0700

    Tugas 12 Javascript
    tugas untuk Niomic

---
+++ tugas12.js
@@ -0,0 +1,10 @@
+function tugasDuaBelas(){
+	let buah = ['pisang','Jeruk','Apel','Mangga'];
+	console.log(buah);
+	buah.pop();
+	console.log(buah);
+	buah.shift();
+	console.log(buah);
+}
+
+tugasDuaBelas();
